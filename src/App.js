933c67bab67744cd0c475070719107e910bee4a8
AtiwatPassara <112745033+AtiwatPassara@users.noreply.github.com>
2023-12-15 21:29:30 +0700

    Icon update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
